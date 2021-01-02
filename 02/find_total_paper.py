b9acb65aaca6be58c2e3f99e97918e48558108c0
bliutwo <bliutwo@cs.stanford.edu>
2021-01-01 23:17:57 -0800

    Complete Part 2 of Day 2

--- 02/find_total_paper.py
+++ 02/find_total_paper.py
@@ -5,19 +5,19 @@ assert(strings)
 # print(strings)
 strings = strings.split('\n')
 total = 0
+ribbon = 0
 for line in strings:
     if line:
         dimensions = line.split('x')
-        # print(dimensions)
         dimensions = [int(n) for n in dimensions]
         dimensions.sort()
         assert(len(dimensions) == 3)
         l, w, h = dimensions
-        # print(l, w, h)
         surface_area = 2*l*w + 2*w*h + 2*h*l
         smallest = l*w
-        # print(surface_area)
-        # print(smallest)
         total += surface_area + smallest
-        # print(total)
+        wrap = l + l + w + w
+        bow  = l*w*h
+        ribbon += wrap + bow
 print(total)
+print(ribbon)
