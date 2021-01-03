84a10a861685a2cad388414641c6fee24df258d4
bliutwo <bliutwo@cs.stanford.edu>
2021-01-03 00:47:35 -0800

    Solve part 2 of Day 4

--- 04/solution.py
+++ 04/solution.py
@@ -4,11 +4,11 @@ import hashlib
 # it combines with to produce an MD5 hash starting with
 # at least five zeroes
 def produce_answer(secret_key):
-    for i in range(100000000):
+    for i in range(1000000000000):
         to_check = secret_key + str(i)
         result = hashlib.md5(to_check.encode())
         str_result = result.hexdigest()
-        if len(str_result) >= 5 and str_result[0:5] == "00000":
+        if len(str_result) >= 5 and str_result[0:6] == "000000":
             print(i)
             return str(i)
     print("not found")
@@ -18,7 +18,7 @@ actual_input = "iwrupvqb"
 
 samples_and_answers = {"abcdef": "609043", "pqrstuv": "1048970"}
 
-for sample, answer in samples_and_answers.items():
-    print(produce_answer(sample) == answer)
+# for sample, answer in samples_and_answers.items():
+#     print(produce_answer(sample) == answer)
 
 print(produce_answer(actual_input))
